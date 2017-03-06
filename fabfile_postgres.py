6ef16012f2febc4a6eb98d1a2044ed7c3682e9ef
Rinat Khabibiev <srenskiy@gmail.com>
2017-03-07 00:19:00 +0300

    improved postgres configuration

--- fabfile_postgres.py
+++ fabfile_postgres.py
@@ -4,17 +4,18 @@ from fabricio.misc import AvailableVagrantHosts
 
 all_hosts = AvailableVagrantHosts(guest_network_interface='eth1')
 
-db = tasks.DockerTasks(
+postgres = tasks.DockerTasks(
     service=StreamingReplicatedPostgresqlContainer(
         name='postgres',
         image='postgres:9.6-alpine',
-        pg_data='/data',
+        pg_data='/data/fabricio_postgres',
         pg_hba='postgres/pg_hba.conf',
         pg_conf='postgres/postgresql.conf',
         pg_recovery='postgres/recovery.conf',
         pg_recovery_master_promotion_enabled=True,
+        pg_recovery_wait_for_master_seconds=5,
         options=dict(
-            volume='/data:/data',
+            volume='/data/fabricio_postgres:/data',
             env='PGDATA=/data',
             publish='5432:5432',
         ),
